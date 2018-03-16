5338c6695e2b4f3ecb082db5c3596ba15d42d7f9
Zektra <mickeyspiggy@gmail.com>
2018-03-16 12:52:22 +0100

    Delete some unnessecary files, ADD FIRST MOBILE GAME

---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour {
+
+         
+        float start;
+        float end;
+        float nextActionTime;
+        Renderer rend;
+	// Use this for initialization
+	void Start ()
+        {
+            resetSwipe();
+            rend = GetComponent<Renderer>();
+            rend.material.shader = Shader.Find("Specular");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+        {
+            if (Time.time >= nextActionTime)
+            {
+                rend.material.SetColor("_SpecColor", Color.red);
+                ListenSwipe();
+            }
+	}
+
+        void ListenSwipe()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                start = Input.mousePosition.x;
+            }
+            if (Input.GetButtonUp("Fire1"))
+            {
+                end = Input.mousePosition.x;
+            }
+            if (end - start > 0 && start != 0 && end != 0)
+            {
+                rend.material.SetColor("_SpecColor", Color.black);
+                resetSwipe();
+            }
+         }
+
+        void resetSwipe()
+        {
+            start = 0;
+            end = 0;
+            nextActionTime = Time.time + Random.Range(1, 5);
+        }
+}
